5d139197f80aa01165c530aaa2080124f2d461d8
zhangwd5173 <zhangwd5173@nokee.com>
2015-06-23 11:02:06 +0800

    抽屉效果

---
+++ public/js/custom.js
@@ -0,0 +1,31 @@
+﻿jQuery(document).ready(function($) {
+	"use strict";
+	/* Load PhotoSwipes (Image galleries) */
+	var options = {}; // Set the options to a variable
+	if ($('#gallery-swiper a').length) { // Check if the targetted element exists (avoiding console errors)
+		$('#gallery-swiper a').photoSwipe(options); // Call the initializer function
+	}
+	if ($('.gallery a').length)	{
+		$('.gallery a').photoSwipe(options);
+	}
+	if ($('.photography a').length)	{
+		$('.photography a').photoSwipe(options);
+	}
+	
+	/* Notification Box events */
+	$('.notification').append('<div class="close">X</div>');
+	$('.notification .close').click(function(){
+		$(this).parent().animate({opacity: "0"});
+		$(this).parent().slideUp();
+	});
+	
+	/* Vertical aligner (used in signin.html) */
+	$(window).resize(function() {
+		var mt = ($('body').height() / 2) - ($('.content').height() / 2) - 30;
+		if (mt < 8) mt = 8;
+		$('.vertical-centered').css({
+			marginTop: mt + "px"
+		});
+	});
+	$(window).resize();
+});
---
+++ public/js/swipers.js
@@ -0,0 +1,31 @@
+﻿(function($) {
+	"use strict";
+	/* Swiper declaration (index.html) */
+	var sliderSwiper = new Swiper('#slider-swiper',{
+		loop: true,
+		grabCursor: true,
+		calculateHeight: true,
+		autoResize: true,
+		autoplay: 5000
+	});
+	/* Height fix for fluid swipers when orieantation changes */
+	$(window).resize(function(){
+		if ($('#slider-swiper').length) { // Check if the targetted element exists (avoiding console errors)
+			sliderSwiper.reInit();
+		}
+	});
+
+	/* Swiper declaration (gallery.html) */
+	var gallerySwiper = new Swiper('#gallery-swiper',{
+		grabCursor: true,
+		calculateHeight: true,
+		freeMode: true,
+		freeModeFluid: true,
+		slidesPerView: 'auto',
+		scrollbar: {
+			container :'#gallery-swiper-scrollbar',
+			hide: false,
+			draggable: true  
+		}
+	});
+})(jQuery);
